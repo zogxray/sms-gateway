28b89a9463e99adbcc35d72a1adf9dea65b7f645
zogxray <zogxray@gmail.com>
2018-08-29 14:18:31 +0300

    master sent_at fix

--- models/sms.py
+++ models/sms.py
@@ -4,7 +4,7 @@ from orator.orm import belongs_to
 class Sms(Model):
     __table__ = 'sms'
     __fillable__ = ['phone', 'text', 'channel_id', 'sim_msg_count', 'direction', 'send_at', 'received_at']
-    __dates__ = ['created_at', 'updated_at', 'send_at']
+    __dates__ = ['created_at', 'updated_at', 'send_at', 'received_at']
 
     @belongs_to
     def channel(self):
